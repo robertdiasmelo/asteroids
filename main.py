d116d4ef47473bae3ce659988f1892dce52b2880
robertdiasmelo <crobertdmelo@gmail.com>
2025-02-01 19:29:52 -0300

    edit files indentation and formating

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,7 +14,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
 
         screen.fill("black")
         player.draw(screen)
